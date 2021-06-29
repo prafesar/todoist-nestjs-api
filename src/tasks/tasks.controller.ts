c104d2d8b00c350672286898dce430a5f7210bd7
Denis Blagaveshchanski <prafesar@tut.by>
2021-06-29 14:36:50 +0300

    init TasksController

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Param, Get } from '@nestjs/common';
+
+import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.getTaskById(id);
+  }
+}
