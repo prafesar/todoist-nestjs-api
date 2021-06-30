1f464a13b34b3837094eb8a4db3b1400355a54f2
Denis Blagaveshchanski <prafesar@tut.by>
2021-06-30 10:04:05 +0300

    task repo: create methods

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,9 +1,7 @@
-import { Get, Param } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
-import { TaskPriority } from './task-priority.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
@@ -11,5 +9,37 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, priority, search } = filterDto;
+
+    const query = this.createQueryBuilder('task');
+
+    status && query.andWhere('task.status = :status', { status });
+    priority && query.andWhere('task.priority = :priority', { priority });
+    
+    if (search) {
+      query.andWhere(
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    });
+
+    await this.save(task);
+    return task;
+  }
 
 }
