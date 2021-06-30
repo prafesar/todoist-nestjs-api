43751865bb3ab4274cb24b5aa8cd496dbfa8009a
Denis Blagaveshchanski <prafesar@tut.by>
2021-06-30 10:05:16 +0300

    tasks.service: add methods

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
-import { Task } from './task.entity'
-import { TasksRepository } from './tasks.repository';
+import { Task } from './task.entity';
+import { TaskPriority } from './task-priority.enum';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TasksRepository } from './tasks.repository';
 
 @Injectable()
 export class TasksService {
@@ -11,6 +14,10 @@ export class TasksService {
     @InjectRepository(TasksRepository)
     private tasksRepository: TasksRepository,
   ) {}
+
+  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.tasksRepository.getTasks(filterDto);
+  }
 
   async getTaskById(id: string): Promise<Task> {
     const found = await this.tasksRepository.findOne(id);
@@ -20,8 +27,34 @@ export class TasksService {
     return found;
   }
 
-  // createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
-  //   return this.tasksRepository.createTask(createTaskDto, user);
-  // }
+  createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksRepository.createTask(createTaskDto);
+    // TODO: need author ID
+  }
 
+  async deleteTask(id: string): Promise<void> {
+    const result = await this.tasksRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+  }
+
+  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+
+    task.status = status;
+    await this.tasksRepository.save(task);
+
+    return task;
+  }
+
+  async updateTaskPriority(id: string, priority: TaskPriority): Promise<Task> {
+    const task = await this.getTaskById(id);
+
+    task.priority = priority;
+    await this.tasksRepository.save(task);
+
+    return task;
+  }
 }
