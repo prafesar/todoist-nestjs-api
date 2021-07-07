97d6944c80feb5e90e489947aa6247e80e24d7bd
Denis Blagaveshchanski <prafesar@tut.by>
2021-07-07 18:00:01 +0300

    style RolesGuard

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -6,18 +6,21 @@ import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    ) {}
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndMerge<UserRole[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
+    
     if (!requiredRoles) {
       return true;
     }
+    
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
-    // return true;
+    return requiredRoles.some((role) => user.role === role);
   }
 }
