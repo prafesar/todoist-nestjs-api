62ba746d5f274f8f337cc00920a3e6391e30db6c
prafesar <prafesar@tut.by>
2021-07-28 16:41:18 +0300

    add comment mappers

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult } from 'typeorm';
 
 import { CommentEntity } from '../comments/comment.entity';
 import { CommentsRepository } from '../comments/comments.repository';
@@ -8,6 +9,8 @@ import { UpdateCommentDto } from '../comments/dto/update-comment.dto';
 import { NotifyService } from '../notify/notify.service';
 import { UserEntity } from '../users/user.entity';
 import { UsersService } from '../users/users.service';
+import { CommentListResponseInterface } from './types/comment-list-response.interface';
+import { CommentResponseInterface } from './types/comment-response.interface';
 
 @Injectable()
 export class CommentsService {
@@ -22,25 +25,24 @@ export class CommentsService {
     createCommentDto: CreateCommentDto,
   ): Promise<CommentEntity> {
 
-    const comment = this.commentsRepository.create({
-      ...createCommentDto,
-    })
+    const comment = await this.commentsRepository.createComment(createCommentDto)
+    return await this.commentsRepository.saveComment(comment);
     // if mentioned, send reminder
-    const { description } = comment;
-    const mentionedUsers: Promise<UserEntity>[] = description
-      .split(' ')
-      .filter(word => word[0] === '@')
-      .map(item => this.usersService.getUserByLogin(item.substring(1)))
+    // const { description } = comment;
+    // const mentionedUsers: Promise<UserEntity>[] = description
+    //   .split(' ')
+    //   .filter(word => word[0] === '@')
+    //   .map(item => this.usersService.getUserByLogin(item.substring(1)))
 
-    Promise.all(mentionedUsers)
-      .then(users => users.forEach(({ email }) => {
-        this.notifyService.sendLetter({ to: email, subject: '' , text: description});
-      }))
-    return comment;
+    // Promise.all(mentionedUsers)
+    //   .then(users => users.forEach(({ email }) => {
+    //     this.notifyService.sendLetter({ to: email, subject: '' , text: description});
+    //   }))
+    
   }
 
   async getCommentById(id: string): Promise<CommentEntity> {
-    const found = await this.commentsRepository.findOne(id);
+    const found = await this.commentsRepository.findCommentById(id);
     if (!found) {
       throw new NotFoundException(`Comment with ID: ${id} not found`);
     }
@@ -53,6 +55,7 @@ export class CommentsService {
     currUser: UserEntity,
   ): Promise<CommentEntity> {
     // need check if owner
+    
     const comment = await this.commentsRepository.preload({
       id,
       ...commentDto,
@@ -63,14 +66,33 @@ export class CommentsService {
     return this.commentsRepository.save(comment);
   }
 
-  async deleteCommentById(id: string): Promise<void> {
-    const result = await this.commentsRepository.delete(id);
+  async removeCommentById(id: string): Promise<DeleteResult> {
+    const result = await this.commentsRepository.removeCommentById(id);
 
     if (result.affected === 0) {
       throw new NotFoundException(`Comments with ID "${id}" not found`);
     }
+    return result;
   }
 
+  buildCommentResponse(comment: CommentEntity): CommentResponseInterface {
+    const { author, task, ...rest } = comment;
+    return {
+      comment: {
+        ...rest,
+        authorId: author.id,
+        taskId: task.id,
+      }
+    };
+  }
+
+  buildCommentListResponse(comments: CommentEntity[]): CommentListResponseInterface {
+    const result = comments.map(item => this.buildCommentResponse(item).comment)
+    return {
+      comments: result,
+      count: result.length,
+    }
+  }
 }
 
 
